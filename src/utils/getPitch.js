6b1d6104a82197b6aa67fd4dbd9eacae81943a74
christian-spooner <cswspooner@gmail.com>
2022-10-31 21:04:59 +0000

    voice memo

---
+++ src/utils/getPitch.js
@@ -0,0 +1,9 @@
+import * as Pitchfinder from "pitchfinder";
+
+const myAudioBuffer = getAudioBuffer(); // assume this returns a WebAudio AudioBuffer object
+const float32Array = myAudioBuffer.getChannelData(0); // get a single channel of sound
+
+const detectPitch = Pitchfinder.AMDF();
+const pitch = detectPitch(float32Array); // null if pitch cannot be identified
+
+console.log(pitch)
