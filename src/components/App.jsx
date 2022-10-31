c2e52c79f55e0123ee6e12b55daa36e4e3b8f665
christian-spooner <cswspooner@gmail.com>
2022-10-31 21:59:10 +0000

    react audio record

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,61 @@
 import './App.css'
 
 function App() {
+  let pitch = 10
+  const audioCtx = new AudioContext()
+  let recording = false
+  let recordElement = <p><button id="record">Record</button></p>
+  let pitchElement = <div id="pitch"></div>
+
+  // Ask user for access to the microphone.
+  if (navigator.mediaDevices) {
+    navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
+
+      // Instantiate the media recorder.
+      const mediaRecorder = new MediaRecorder(stream)
+
+      // Create a buffer to store the incoming data.
+      let chunks = []
+      mediaRecorder.ondataavailable = (event) => {
+        chunks.push(event.data)
+      }
+
+    mediaRecorder.onstop = (event) => {
+      const audio = new Audio()
+      audio.setAttribute("controls", "")
+      pitchElement.append(audio)
+      pitchElement.append("<br />")
+
+      // Combine the audio chunks into a blob, then point the empty audio clip to that blob.
+      const blob = new Blob(chunks, {"type": "audio/ogg codecs=opus"})
+      audio.src = window.URL.createObjectURL(blob)
+      chunks = []
+    }
+
+    recordElement.on("click", () => {
+      if (recording) {
+        mediaRecorder.stop()
+        recording = false
+        recordElement.html("Record")
+      } else {
+        mediaRecorder.start()
+        recording = true
+        recordElement.html("Stop")
+      }
+    })
+
+    }).catch((err) => {
+      alert("Cannot access your computer's microphone.")
+    })
+  } else {
+    alert("Cannot access your computer's microphone. Please update your browser.")
+  }
+
   return (
     <div className="App">
-      Pitch
+      <h1>Vocal Pitch Monitor</h1>
+      {recordElement}
+      {pitchElement}
     </div>
   )
 }
--- src/utils/getPitch.js
+++ src/utils/getPitch.js
@@ -1,5 +1,7 @@
 import * as Pitchfinder from "pitchfinder";
 
+
+
 const myAudioBuffer = getAudioBuffer(); // assume this returns a WebAudio AudioBuffer object
 const float32Array = myAudioBuffer.getChannelData(0); // get a single channel of sound
 
