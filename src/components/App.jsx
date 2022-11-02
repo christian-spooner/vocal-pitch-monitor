aa2880542c0f2f7905d291dded3a9141ae3737d1
christian-spooner <cswspooner@gmail.com>
2022-11-02 20:16:22 +0000

    add pitch detection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,79 @@ import './App.css'
 import React, { useState } from 'react'
 
 function App() {
-  let [recording, setRecording] = useState("Start");
-  let [pitch, setPitch] = useState(0);
+  let [recording, setRecording] = useState("Start")
+  let [pitch, setPitch] = useState(0)
+
+  function getAutocorrolatedPitch()
+  {
+    // First: autocorrolate the signal
+
+    let maximaCount = 0
+
+    for (let l = 0; l < analyserNode.fftSize; l++) 
+    {
+      corrolatedSignal[l] = 0
+      for (let i = 0; i < analyserNode.fftSize - l; i++) 
+      {
+        corrolatedSignal[l] += audioData[i] * audioData[i + l]
+      }
+      if (l > 1) 
+      {
+        if ((corrolatedSignal[l - 2] - corrolatedSignal[l - 1]) < 0
+          && (corrolatedSignal[l - 1] - corrolatedSignal[l]) > 0) 
+        {
+          localMaxima[maximaCount] = (l - 1)
+          maximaCount++
+          if ((maximaCount >= localMaxima.length))
+            break
+        }
+      }
+    }
+
+    // Second: find the average distance in samples between maxima
+
+    let maximaMean = localMaxima[0]
+
+    for (let i = 1; i < maximaCount; i++)
+      maximaMean += localMaxima[i] - localMaxima[i - 1]
+
+    maximaMean /= maximaCount
+
+    return audioCtx.sampleRate / maximaMean
+  }
+
+  function startPitchDetection() 
+  {
+    navigator.mediaDevices.getUserMedia ({audio: true})
+        .then((stream) =>
+        {
+            microphoneStream = audioCtx.createMediaStreamSource(stream)
+            microphoneStream.connect(analyserNode)
+
+            audioData = new Float32Array(analyserNode.fftSize)
+            corrolatedSignal = new Float32Array(analyserNode.fftSize)
+
+            setInterval(() => 
+            {
+                analyserNode.getFloatTimeDomainData(audioData)
+                let pitch = getAutocorrolatedPitch()
+                setPitch(pitch)
+            }, 300)
+        })
+        .catch((err) =>
+        {
+            console.log(err)
+        })
+  }
   
-  function recordHandler() {
-    if (recording == "Stop") {
+  function recordHandler() 
+  {
+    if (recording == "Stop") 
+    {
       setRecording("Start") 
     } else {
       setRecording("Stop")
+      startPitchDetection()
     }
     return
   }
