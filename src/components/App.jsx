de2c0055c82eb35357f90301386dc5f5a0823ca8
christian-spooner <cswspooner@gmail.com>
2022-11-05 19:49:10 +0000

    attempt to start/stop monitoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,97 +1,101 @@
-import './App.css'
-import React, { useState } from 'react'
+import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  let [recording, setRecording] = useState("Start")
-  let [frequency, setFrequency] = useState(0)
-  let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  let microphoneStream = null;
-  let analyserNode = audioCtx.createAnalyser()
-  let audioData = new Float32Array(analyserNode.fftSize);;
-  let corrolatedSignal = new Float32Array(analyserNode.fftSize);;
-  let localMaxima = new Array(10);
+let monitorInterval;
 
-  function getAutocorrolatedPitch()
-  {
-    // First: autocorrolate the signal
+function runMonitor() {
+    let audioCtx = new AudioContext();
+    let microphoneStream = null;
+    let analyserNode = audioCtx.createAnalyser();
+    let audioData = new Float32Array(analyserNode.fftSize);
+    let corrolatedSignal = new Float32Array(analyserNode.fftSize);
+    let localMaxima = new Array(10);
 
-    let maximaCount = 0;
+    function getAutocorrolatedPitch() {
+        let maximaCount = 0;
 
-    for (let l = 0; l < analyserNode.fftSize; l++) {
-      corrolatedSignal[l] = 0;
-      for (let i = 0; i < analyserNode.fftSize - l; i++) {
-        corrolatedSignal[l] += audioData[i] * audioData[i + l];
-      }
-      if (l > 1) {
-        if ((corrolatedSignal[l - 2] - corrolatedSignal[l - 1]) < 0
-          && (corrolatedSignal[l - 1] - corrolatedSignal[l]) > 0) {
-          localMaxima[maximaCount] = (l - 1);
-          maximaCount++;
-          if ((maximaCount >= localMaxima.length))
-            break;
+        for (let l = 0; l < analyserNode.fftSize; l++) {
+            corrolatedSignal[l] = 0;
+            for (let i = 0; i < analyserNode.fftSize - l; i++) {
+                corrolatedSignal[l] += audioData[i] * audioData[i + l];
+            }
+            if (l > 1) {
+                if (
+                    corrolatedSignal[l - 2] - corrolatedSignal[l - 1] < 0 &&
+                    corrolatedSignal[l - 1] - corrolatedSignal[l] > 0
+                ) {
+                    localMaxima[maximaCount] = l - 1;
+                    maximaCount++;
+                    if (maximaCount >= localMaxima.length) break;
+                }
+            }
         }
-      }
+
+        let maximaMean = localMaxima[0];
+
+        for (let i = 1; i < maximaCount; i++)
+            maximaMean += localMaxima[i] - localMaxima[i - 1];
+
+        maximaMean /= maximaCount;
+
+        return audioCtx.sampleRate / maximaMean;
     }
 
-    // Second: find the average distance in samples between maxima
+    function getPitch() {
+        navigator.mediaDevices
+            .getUserMedia({ audio: true })
+            .then((stream) => {
+                microphoneStream = audioCtx.createMediaStreamSource(stream);
+                microphoneStream.connect(analyserNode);
 
-    let maximaMean = localMaxima[0];
-
-    for (let i = 1; i < maximaCount; i++)
-      maximaMean += localMaxima[i] - localMaxima[i - 1];
-
-    maximaMean /= maximaCount;
-
-    return audioCtx.sampleRate / maximaMean;
-  }
-
-  function startPitchDetection()
-  {
-    navigator.mediaDevices.getUserMedia ({audio: true})
-      .then((stream) =>
-      {
-        microphoneStream = audioCtx.createMediaStreamSource(stream);
-        microphoneStream.connect(analyserNode);
-
-        audioData = new Float32Array(analyserNode.fftSize);
-        corrolatedSignal = new Float32Array(analyserNode.fftSize);
-
-        setInterval(() => {
-          analyserNode.getFloatTimeDomainData(audioData);
-
-          let pitch = getAutocorrolatedPitch();
-
-          setFrequency(pitch);
-        }, 300);
-      })
-      .catch((err) =>
-      {
-        console.log(err);
-      });
-  }
-  
-  function recordHandler() 
-  {
-    if (recording == "Stop") 
-    {
-      setRecording("Start") 
-    } else {
-      setRecording("Stop")
-      startPitchDetection()
+                audioData = new Float32Array(analyserNode.fftSize);
+                corrolatedSignal = new Float32Array(analyserNode.fftSize);
+                analyserNode.getFloatTimeDomainData(audioData);
+                return getAutocorrolatedPitch();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
-    return
-  }
 
-  let recordElement = <p><button id="record" onClick={recordHandler}>{recording}</button></p>
-  let frequencyElement = <div id="pitch">Frequency: {frequency}</div>
-
-  return (
-    <div className="App">
-      <h1>Vocal Pitch Monitor</h1>
-      {recordElement}
-      {frequencyElement}
-    </div>
-  )
+    return getPitch();
 }
 
-export default App
+function App() {
+    let [recording, setRecording] = useState('Start');
+    let [frequency, setFrequency] = useState(0);
+
+    function recordHandler() {
+        if (recording == 'Stop') {
+            clearInterval(monitorInterval);
+            monitorInterval = null;
+            setRecording('Start');
+        } else {
+            setRecording('Stop');
+            if (!monitorInterval) 
+            {
+                monitorInterval = setInterval(runMonitor, 1000);
+            }
+        }
+        return;
+    }
+
+    let recordElement = (
+        <p>
+            <button id="record" onClick={recordHandler}>
+                {recording}
+            </button>
+        </p>
+    );
+    let frequencyElement = <div id="pitch">{frequency}</div>;
+
+    return (
+        <div className="App">
+            <h1>Vocal Pitch Monitor</h1>
+            {recordElement}
+            {frequencyElement}
+        </div>
+    );
+}
+
+export default App;
