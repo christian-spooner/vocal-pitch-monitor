7ae9e183dcba9d3bca96168b338d3d980a224829
christian-spooner <93479191+christian-spooner@users.noreply.github.com>
2022-12-04 17:06:48 +0000

    Refactoring checkpoint

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-function start() {
-    console.log("starting...")
-}
-
 function init() {
     var source;
     var audioContext = new (window.AudioContext || window.webkitAudioContext)();
     var analyser = audioContext.createAnalyser();
+
     analyser.minDecibels = -100;
     analyser.maxDecibels = -10;
     analyser.smoothingTimeConstant = 0.85;
+
     if (!navigator?.mediaDevices?.getUserMedia) {
       // No audio allowed
       alert('Sorry, getUserMedia is required for the app.')
@@ -18,82 +16,25 @@ function init() {
       navigator.mediaDevices.getUserMedia(constraints)
         .then(
           function(stream) {
-            // Initialize the SourceNode
             source = audioContext.createMediaStreamSource(stream);
-            // Connect the source node to the analyzer
             source.connect(analyser);
             visualize();
           }
         )
         .catch(function(err) {
           console.log(err)
-          alert('Sorry, microphone permissions are required for the app. Feel free to read on without playing :)')
+          alert('Sorry, microphone permissions are required for the app')
         });
     }
-  
-    // Visualizing, copied from voice change o matic
-    var canvas = document.querySelector('.visualizer');
-    var canvasContext = canvas.getContext("2d");
-    var WIDTH;
-    var HEIGHT;
-  
+    
     function visualize() {
-      WIDTH = canvas.width;
-      HEIGHT = canvas.height;
-  
-      var drawVisual;
-      var drawNoteVisual;
-  
-      var draw = function() {
-        drawVisual = requestAnimationFrame(draw);
-        analyser.fftSize = 2048;
-        var bufferLength = analyser.fftSize;
-        var dataArray = new Uint8Array(bufferLength);
-        analyser.getByteTimeDomainData(dataArray);
-  
-        canvasContext.fillStyle = 'rgb(200, 200, 200)';
-        canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
-  
-        canvasContext.lineWidth = 2;
-        canvasContext.strokeStyle = 'rgb(0, 0, 0)';
-  
-        canvasContext.beginPath();
-  
-        var sliceWidth = WIDTH * 1.0 / bufferLength;
-        var x = 0;
-  
-        for(var i = 0; i < bufferLength; i++) {
-  
-          var v = dataArray[i] / 128.0;
-          var y = v * HEIGHT/2;
-  
-          if(i === 0) {
-            canvasContext.moveTo(x, y);
-          } else {
-            canvasContext.lineTo(x, y);
-          }
-  
-          x += sliceWidth;
-        }
-  
-        canvasContext.lineTo(canvas.width, canvas.height/2);
-        canvasContext.stroke();
-      }
-  
       var previousValueToDisplay = 0;
       var smoothingCount = 0;
       var smoothingThreshold = 5;
       var smoothingCountThreshold = 5;
   
-      // Thanks to PitchDetect: https://github.com/cwilso/PitchDetect/blob/master/js/pitchdetect.js
-      var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-      function noteFromPitch( frequency ) {
-        var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
-        return Math.round( noteNum ) + 69;
-      }
-  
       var drawNote = function() {
-        drawNoteVisual = requestAnimationFrame(drawNote);
+        requestAnimationFrame(drawNote);
         var bufferLength = analyser.fftSize;
         var buffer = new Float32Array(bufferLength);
         analyser.getFloatTimeDomainData(buffer);
@@ -101,34 +42,15 @@ function init() {
   
         // Handle rounding
         var valueToDisplay = autoCorrelateValue;
-        var roundingValue = "none"
-        if (roundingValue == 'none') {
-          // Do nothing
-        } else if (roundingValue == 'hz') {
-          valueToDisplay = Math.round(valueToDisplay);
-        } else {
-          // Get the closest note
-          // Thanks to PitchDetect:
-          valueToDisplay = noteStrings[noteFromPitch(autoCorrelateValue) % 12];
-        }
-  
-        var smoothingValue = "none"
-  
-  
+
         if (autoCorrelateValue === -1) {
           document.getElementById('note').innerText = 'Too quiet...';
           return;
         }
-        if (smoothingValue === 'none') {
-          smoothingThreshold = 99999;
-          smoothingCountThreshold = 0;
-        } else if (smoothingValue === 'basic') {
-          smoothingThreshold = 10;
-          smoothingCountThreshold = 5;
-        } else if (smoothingValue === 'very') {
-          smoothingThreshold = 5;
-          smoothingCountThreshold = 10;
-        }
+
+        smoothingThreshold = 99999;
+        smoothingCountThreshold = 0;
+
         function noteIsSimilarEnough() {
           // Check threshold for number, or just difference for notes.
           if (typeof(valueToDisplay) == 'number') {
@@ -137,6 +59,7 @@ function init() {
             return valueToDisplay === previousValueToDisplay;
           }
         }
+        
         // Check if this value has been within the given range for n iterations
         if (noteIsSimilarEnough()) {
           if (smoothingCount < smoothingCountThreshold) {
@@ -151,58 +74,20 @@ function init() {
           smoothingCount = 0;
           return;
         }
+
         if (typeof(valueToDisplay) == 'number') {
+          valueToDisplay = Math.round(valueToDisplay)
           valueToDisplay += ' Hz';
         }
   
         document.getElementById('note').innerText = valueToDisplay;
       }
   
-      var drawFrequency = function() {
-        var bufferLengthAlt = analyser.frequencyBinCount;
-        var dataArrayAlt = new Uint8Array(bufferLengthAlt);
-  
-        canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
-  
-        var drawAlt = function() {
-          drawVisual = requestAnimationFrame(drawAlt);
-  
-          analyser.getByteFrequencyData(dataArrayAlt);
-  
-          canvasContext.fillStyle = 'rgb(0, 0, 0)';
-          canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
-  
-          var barWidth = (WIDTH / bufferLengthAlt) * 2.5;
-          var barHeight;
-          var x = 0;
-  
-          for(var i = 0; i < bufferLengthAlt; i++) {
-            barHeight = dataArrayAlt[i];
-  
-            canvasContext.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
-            canvasContext.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
-  
-            x += barWidth + 1;
-          }
-        };
-  
-        console.log('wut')
-        drawAlt();
-      }
-  
-      var displayValue = "sine"
-      if (displayValue == 'sine') {
-        draw();
-      } else {
-        drawFrequency();
-      }
       drawNote();
     }
 }
-  
-  
-// Must be called on analyser.getFloatTimeDomainData and audioContext.sampleRate
-// From https://github.com/cwilso/PitchDetect/pull/23
+
+
 function autoCorrelate(buffer, sampleRate) {
     // Perform a quick root-mean-square to see if we have enough signal
     var SIZE = buffer.length;
