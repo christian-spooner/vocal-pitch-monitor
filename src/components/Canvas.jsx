85426f6fa11dfe43f9da74553686b8a105c5ad59
christian-spooner <93479191+christian-spooner@users.noreply.github.com>
2022-12-06 21:44:09 +0000

    Basic axes

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -8,18 +8,36 @@ const Canvas = ({ frequency }) => {
         const context = canvas.getContext('2d');
 
         // Set up canvas dimensions and clear previous drawings
-        canvas.width = 500;
-        canvas.height = 1000;
+        canvas.width = 1200;
+        canvas.height = 800;
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         context.beginPath();
         context.moveTo(canvas.width / 2, 0);
         context.lineTo(canvas.width / 2, canvas.height);
-        context.strokeStyle = 'black';
+        context.strokeStyle = 'white';
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(0, canvas.height);
+        context.lineTo(canvas.width, canvas.height);
+        context.strokeStyle = 'white';
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(0, 0);
+        context.lineTo(0, canvas.height);
+        context.strokeStyle = 'white';
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(canvas.width, 0);
+        context.lineTo(canvas.width, canvas.height);
+        context.strokeStyle = 'white';
         context.stroke();
 
         // Calculate dot's y-coordinate based on frequency
-        const y = 1000 - frequency;
+        const y = 800 - frequency;
 
         // Draw dot at calculated y-coordinate
         context.beginPath();
