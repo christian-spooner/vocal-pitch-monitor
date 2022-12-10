1ecbff8edf97979f45d64c36cfdf788341f5797c
christian-spooner <93479191+christian-spooner@users.noreply.github.com>
2022-12-10 22:45:54 +0000

    Linting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,98 +4,107 @@ import PitchGraph from './PitchGraph';
 import autoCorrelate from '../utils/autoCorrelate';
 
 function App() {
-    let [freq, useFreq] = useState(0)
+    let [freq, useFreq] = useState(0);
 
     function record() {
-      var source;
-      var audioContext = new window.AudioContext();
-      var analyser = audioContext.createAnalyser();
-    
-      analyser.minDecibels = -100;
-      analyser.maxDecibels = -10;
-      analyser.smoothingTimeConstant = 0.85;
-    
-      if (!navigator?.mediaDevices?.getUserMedia) {
-        // No audio allowed
-        alert('Sorry, getUserMedia is required for the app.')
-        return;
-      } else {
-        var constraints = {audio: true};
-        navigator.mediaDevices.getUserMedia(constraints)
-          .then(
-            function(stream) {
-              source = audioContext.createMediaStreamSource(stream);
-              source.connect(analyser);
-              visualize();
-            }
-          )
-          .catch(function(err) {
-            console.log(err)
-            alert('Sorry, microphone permissions are required for the app')
-          });
-      }
-      
-      function visualize() {
-        var previousValueToDisplay = 0;
-        var smoothingCount = 0;
-        var smoothingThreshold = 5;
-        var smoothingCountThreshold = 5;
-    
-        var drawNote = function() {
-          requestAnimationFrame(drawNote);
-          var bufferLength = analyser.fftSize;
-          var buffer = new Float32Array(bufferLength);
-          analyser.getFloatTimeDomainData(buffer);
-          var autoCorrelateValue = autoCorrelate(buffer, audioContext.sampleRate)
-    
-          // Handle rounding
-          var valueToDisplay = autoCorrelateValue;
-    
-          if (autoCorrelateValue === -1) {
-            return -1;
-          }
-    
-          smoothingThreshold = 10;
-          smoothingCountThreshold = 5;
-    
-          function noteIsSimilarEnough() {
-            // Check threshold for number, or just difference for notes.
-            if (typeof(valueToDisplay) == 'number') {
-              return Math.abs(valueToDisplay - previousValueToDisplay) < smoothingThreshold;
-            } else {
-              return valueToDisplay === previousValueToDisplay;
-            }
-          }
-          
-          // Check if this value has been within the given range for n iterations
-          if (noteIsSimilarEnough()) {
-            if (smoothingCount < smoothingCountThreshold) {
-              smoothingCount++;
-              return;
-            } else {
-              previousValueToDisplay = valueToDisplay;
-              smoothingCount = 0;
-            }
-          } else {
-            previousValueToDisplay = valueToDisplay;
-            smoothingCount = 0;
+        var source;
+        var audioContext = new window.AudioContext();
+        var analyser = audioContext.createAnalyser();
+
+        analyser.minDecibels = -100;
+        analyser.maxDecibels = -10;
+        analyser.smoothingTimeConstant = 0.85;
+
+        if (!navigator?.mediaDevices?.getUserMedia) {
+            // No audio allowed
+            alert('Sorry, getUserMedia is required for the app.');
             return;
-          }
-    
-          useFreq(valueToDisplay.toFixed(2));
+        } else {
+            var constraints = { audio: true };
+            navigator.mediaDevices
+                .getUserMedia(constraints)
+                .then(function (stream) {
+                    source = audioContext.createMediaStreamSource(stream);
+                    source.connect(analyser);
+                    visualize();
+                })
+                .catch(function (err) {
+                    console.log(err);
+                    alert(
+                        'Sorry, microphone permissions are required for the app'
+                    );
+                });
         }
-    
-        drawNote()
-      }
+
+        function visualize() {
+            var previousValueToDisplay = 0;
+            var smoothingCount = 0;
+            var smoothingThreshold = 5;
+            var smoothingCountThreshold = 5;
+
+            var drawNote = function () {
+                requestAnimationFrame(drawNote);
+                var bufferLength = analyser.fftSize;
+                var buffer = new Float32Array(bufferLength);
+                analyser.getFloatTimeDomainData(buffer);
+                var autoCorrelateValue = autoCorrelate(
+                    buffer,
+                    audioContext.sampleRate
+                );
+
+                // Handle rounding
+                var valueToDisplay = autoCorrelateValue;
+
+                if (autoCorrelateValue === -1) {
+                    return -1;
+                }
+
+                smoothingThreshold = 10;
+                smoothingCountThreshold = 5;
+
+                function noteIsSimilarEnough() {
+                    // Check threshold for number, or just difference for notes.
+                    if (typeof valueToDisplay == 'number') {
+                        return (
+                            Math.abs(valueToDisplay - previousValueToDisplay) <
+                            smoothingThreshold
+                        );
+                    } else {
+                        return valueToDisplay === previousValueToDisplay;
+                    }
+                }
+
+                // Check if this value has been within the given range for n iterations
+                if (noteIsSimilarEnough()) {
+                    if (smoothingCount < smoothingCountThreshold) {
+                        smoothingCount++;
+                        return;
+                    } else {
+                        previousValueToDisplay = valueToDisplay;
+                        smoothingCount = 0;
+                    }
+                } else {
+                    previousValueToDisplay = valueToDisplay;
+                    smoothingCount = 0;
+                    return;
+                }
+
+                useFreq(valueToDisplay.toFixed(2));
+            };
+
+            drawNote();
+        }
     }
 
     return (
         <div className="flex flex-col">
-            <button className="py-1" onClick={record}><span className="hover:text-red-600">START</span></button>
+            <button className="py-1" onClick={record}>
+                <span className="hover:text-red-600">START</span>
+            </button>
             <div className="py-1 font-bold">{freq}</div>
-            <PitchGraph frequency={freq}/>
+            <PitchGraph frequency={freq} />
         </div>
-    )
+    );
 }
 
 export default App;
--- src/components/PitchGraph.jsx
+++ src/components/PitchGraph.jsx
@@ -13,19 +13,17 @@ const PitchGraph = ({ frequency }) => {
     const frequencies = Array.from(noteFrequencyMap.values());
     const [data, setData] = useState([]);
 
-    const xScale = d3.scaleLinear()
-        .domain([-4, 0])
-        .range([0, width]);
+    const xScale = d3.scaleLinear().domain([-4, 0]).range([0, width]);
 
-    const yScale = d3.scaleLog()
+    const yScale = d3.scaleLog().domain([minFreq, maxFreq]).range([height, 0]);
+
+    const yScaleRight = d3
+        .scaleLog()
         .domain([minFreq, maxFreq])
         .range([height, 0]);
 
-    const yScaleRight = d3.scaleLog()
-        .domain([minFreq, maxFreq])
-        .range([height, 0]);
-    
-    const line = d3.line()
+    const line = d3
+        .line()
         .x((d) => xScale(d[0]))
         .y((d) => yScale(d[1]))
         .curve(d3.curveMonotoneX);
@@ -33,26 +31,28 @@ const PitchGraph = ({ frequency }) => {
     const updateData = () => {
         // Increment time values by 1
         let newData = data.map((d) => [d[0] - 0.1, d[1]]);
-        
+
         // Add current value of frequency prop to data
         if (frequency > minFreq && frequency < maxFreq) {
-            newData = [...newData, [0, frequency]]
-        } 
+            newData = [...newData, [0, frequency]];
+        }
 
         // Discard data with time values lower than -4
         newData = newData.filter((d) => d[0] >= -4);
-        
+
         setData(newData);
-    }
+    };
 
     const drawGraph = () => {
         d3.select('g').remove(); // remove previous graphs
 
-        const svg = d3.select('.graph')
+        const svg = d3
+            .select('.graph')
             .attr('height', height + margin.top + margin.bottom)
             .attr('width', width + margin.left + margin.right);
 
-        const g = svg.append('g')
+        const g = svg
+            .append('g')
             .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
         // add horizontal lines across the chart that alternate in opacity for each tick of the y-axis
@@ -62,38 +62,37 @@ const PitchGraph = ({ frequency }) => {
             .append('line')
             .attr('x1', 0)
             .attr('x2', width)
-            .attr('y1', frequency => yScaleRight(frequency))
-            .attr('y2', frequency => yScaleRight(frequency))
+            .attr('y1', (frequency) => yScaleRight(frequency))
+            .attr('y2', (frequency) => yScaleRight(frequency))
             .style('stroke', '#ccc')
-            .style('opacity', (frequency, i) => i % 2 === 0 ? 0.2 : 0.6);
+            .style('opacity', (frequency, i) => (i % 2 === 0 ? 0.2 : 0.6));
 
         g.append('g')
             .attr('class', 'x axis')
             .attr('transform', `translate(0, ${height})`)
-            .call(d3.axisBottom(xScale)
-                .tickSize(0)
-                .tickFormat('')
-            );
-        
+            .call(d3.axisBottom(xScale).tickSize(0).tickFormat(''));
+
         g.append('g')
             .attr('class', 'y axis')
             .call(
-                d3.axisLeft(yScale)
-                .tickValues(frequencies)
-                .tickFormat((d) => {
-                    for (let i = 0; i < noteFrequencyTable.length; i++) {
-                        if (noteFrequencyTable[i][1] === d) {
-                            return noteFrequencyTable[i][1];
+                d3
+                    .axisLeft(yScale)
+                    .tickValues(frequencies)
+                    .tickFormat((d) => {
+                        for (let i = 0; i < noteFrequencyTable.length; i++) {
+                            if (noteFrequencyTable[i][1] === d) {
+                                return noteFrequencyTable[i][1];
+                            }
                         }
-                    }
-                })
+                    })
             );
-                
+
         g.append('g')
             .attr('class', 'y axis right')
             .attr('transform', `translate(${width}, 0)`)
             .call(
-                d3.axisRight(yScaleRight)
+                d3
+                    .axisRight(yScaleRight)
                     .tickValues(frequencies)
                     .tickFormat((d) => {
                         for (let i = 0; i < noteFrequencyTable.length; i++) {
@@ -103,24 +102,21 @@ const PitchGraph = ({ frequency }) => {
                         }
                     })
             );
-                
+
         g.append('path')
             .datum(data)
             .attr('class', 'line')
             .attr('d', line)
             .attr('fill', 'none')
             .attr('stroke', 'green');
-    }
+    };
 
     useEffect(() => {
         updateData();
         drawGraph();
     }, [frequency]);
 
-    return (
-        <svg className="graph mx-auto"></svg>
-    );
-}
+    return <svg className="graph mx-auto"></svg>;
+};
 
 export default PitchGraph;
-
