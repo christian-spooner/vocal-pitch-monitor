3308235afea0bffd40adbf8284634719f4190475
christian-spooner <93479191+christian-spooner@users.noreply.github.com>
2022-12-11 16:15:47 +0000

    Function decoupling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,71 @@ import autoCorrelate from '../utils/autoCorrelate';
 
 function App() {
     let [freq, useFreq] = useState(0);
-    let [analyser, useAnalyser] = useState(null);
+    let [analyserProp, useAnalyserProp] = useState(null);
     let [showWinamp, useShowWinamp] = useState(false);
 
     function checkWinamp(event) {
         useShowWinamp(event.target.checked);
     }
+
+	function visualize(analyser, audioContext) {
+		var previousValueToDisplay = 0;
+		var smoothingCount = 0;
+		var smoothingThreshold = 5;
+		var smoothingCountThreshold = 5;
+
+		var drawNote = function () {
+			requestAnimationFrame(drawNote);
+			var bufferLength = analyser.fftSize;
+			var buffer = new Float32Array(bufferLength);
+			analyser.getFloatTimeDomainData(buffer);
+			var autoCorrelateValue = autoCorrelate(
+				buffer,
+				audioContext.sampleRate
+			);
+
+			// Handle rounding
+			var valueToDisplay = autoCorrelateValue;
+
+			if (autoCorrelateValue === -1) {
+				return -1;
+			}
+
+			smoothingThreshold = 10;
+			smoothingCountThreshold = 5;
+
+			function noteIsSimilarEnough() {
+				// Check threshold for number, or just difference for notes.
+				if (typeof valueToDisplay == 'number') {
+					return (
+						Math.abs(valueToDisplay - previousValueToDisplay) <
+						smoothingThreshold
+					);
+				} else {
+					return valueToDisplay === previousValueToDisplay;
+				}
+			}
+
+			// Check if this value has been within the given range for n iterations
+			if (noteIsSimilarEnough()) {
+				if (smoothingCount < smoothingCountThreshold) {
+					smoothingCount++;
+					return;
+				} else {
+					previousValueToDisplay = valueToDisplay;
+					smoothingCount = 0;
+				}
+			} else {
+				previousValueToDisplay = valueToDisplay;
+				smoothingCount = 0;
+				return;
+			}
+
+			useFreq(valueToDisplay.toFixed(2));
+		};
+
+		drawNote();
+	}
 
     function record() {
         var source;
@@ -21,6 +80,7 @@ function App() {
         analyser.minDecibels = -100;
         analyser.maxDecibels = -10;
         analyser.smoothingTimeConstant = 0.85;
+
 
         if (!navigator?.mediaDevices?.getUserMedia) {
             // No audio allowed
@@ -33,8 +93,8 @@ function App() {
                 .then(function (stream) {
                     source = audioContext.createMediaStreamSource(stream);
                     source.connect(analyser);
-                    useAnalyser(analyser);
-                    visualize();
+                    useAnalyserProp(analyser);
+                    visualize(analyser, audioContext);
                 })
                 .catch(function (err) {
                     console.log(err);
@@ -44,64 +104,6 @@ function App() {
                 });
         }
 
-        function visualize() {
-            var previousValueToDisplay = 0;
-            var smoothingCount = 0;
-            var smoothingThreshold = 5;
-            var smoothingCountThreshold = 5;
-
-            var drawNote = function () {
-                requestAnimationFrame(drawNote);
-                var bufferLength = analyser.fftSize;
-                var buffer = new Float32Array(bufferLength);
-                analyser.getFloatTimeDomainData(buffer);
-                var autoCorrelateValue = autoCorrelate(
-                    buffer,
-                    audioContext.sampleRate
-                );
-
-                // Handle rounding
-                var valueToDisplay = autoCorrelateValue;
-
-                if (autoCorrelateValue === -1) {
-                    return -1;
-                }
-
-                smoothingThreshold = 10;
-                smoothingCountThreshold = 5;
-
-                function noteIsSimilarEnough() {
-                    // Check threshold for number, or just difference for notes.
-                    if (typeof valueToDisplay == 'number') {
-                        return (
-                            Math.abs(valueToDisplay - previousValueToDisplay) <
-                            smoothingThreshold
-                        );
-                    } else {
-                        return valueToDisplay === previousValueToDisplay;
-                    }
-                }
-
-                // Check if this value has been within the given range for n iterations
-                if (noteIsSimilarEnough()) {
-                    if (smoothingCount < smoothingCountThreshold) {
-                        smoothingCount++;
-                        return;
-                    } else {
-                        previousValueToDisplay = valueToDisplay;
-                        smoothingCount = 0;
-                    }
-                } else {
-                    previousValueToDisplay = valueToDisplay;
-                    smoothingCount = 0;
-                    return;
-                }
-
-                useFreq(valueToDisplay.toFixed(2));
-            };
-
-            drawNote();
-        }
     }
 
     return (
@@ -121,7 +123,7 @@ function App() {
                     frequency spectrum
                 </label>
             </div>
-            {showWinamp && <Spectrum analyser={analyser} />}
+            {showWinamp && <Spectrum analyser={analyserProp} />}
             <PitchGraph frequency={freq} />
         </div>
     );
